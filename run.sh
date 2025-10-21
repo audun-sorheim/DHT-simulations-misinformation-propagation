494f7ec823a80b6f1341330642cd610c5a4a0abe
audun-sorheim <91456761+audun-sorheim@users.noreply.github.com>
2025-10-21 11:23:34 +0200

    fixed some args

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-Num_LOOPS=${NUM_LOOPS:-1}
+NUM_LOOPS=${NUM_LOOPS:-1}
 
 for ((i=1; i<=NUM_LOOPS; i++)); do
     echo "Starting to run $NUM_LOOPS simulations."
+    K=${K:-$(awk "BEGIN {print 0.1 * ${N:-100}}")}
 
     python3 main.py \
         --N ${N:-100} \
@@ -13,16 +14,17 @@ for ((i=1; i<=NUM_LOOPS; i++)); do
         $( [[ "${CONSP_MEGA_NODE_BOOL:-False}" == "True" ]] && echo "--consp_mega_node_bool" ) \
         --num_iterations ${NUM_ITERATIONS:-150} \
         --num_simulations ${NUM_SIMULATIONS:-200} \
-        --k ${K:-$(echo "0.1 * ${N:-100}" | bc)} \
+        --k ${K} \
         --m ${M:-5} \
         --graph ${GRAPH:-"ER"} \
         --cap ${CAP:-1.0} \
         --sigmoid_factor ${SIGMOID_FACTOR:-4.0} \
-        --std_draw ${STD_DRAW:-0.5} \
+        --std_draw ${STD_DRAW:-1.0} \
         --std_likelihood ${STD_LIKELIHOOD:-0.5} \
         --flex_strength ${FLEX_STRENGTH:-0.8} \
         $( [[ "${LOG_BELIEF_BOOL:-False}" == "True" ]] && echo "--log_belief_bool" ) \
         $( [[ "${CONFBIAS_BOOL:-True}" == "True" ]] && echo "--confbias_bool" )
     LEFT=$((NUM_LOOPS - i))
+    echo "STD_DRAW: ${STD_DRAW}, STD_LIKELIHOOD: ${STD_LIKELIHOOD}"
     echo "Loop number $i is done, $LEFT"
 done
