519dceb781e90e58532610b086679744c5ccd2fd
audun-sorheim <91456761+audun-sorheim@users.noreply.github.com>
2025-12-05 13:08:02 +0100

    new N for f_phase

--- run_many.py
+++ run_many.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 # === CONSTANT DEFAULTS ===
 defaults = {
-    "N": 4096,
+    "N": 1024,
     "K": 10,
     "NUM_ITERATIONS": 200,
     "NUM_SIMULATIONS": 20,
@@ -49,18 +49,18 @@ defaults = {
 # FLEX_STRENGTH_values = [0.98, 0.985, 0.99, 0.995, 0.999, 0.9995, 0.9999]
 # S_values = [10, 100, 1000, 10000, 100000, 1000000]
 
-# start = 0.01
-# end = 0.31
-# # n = 401
-# # step = (end - start) / (n - 1)
-# step = 0.00075
-# n = int((end - start) / step + 1)
-# print(n, type(n))
-# FLEX_STRENGTH_values = [round(start + i * step, 5) for i in range(n)]
-FLEX_STRENGTH_values = [0.00025, 0.001, 0.00175, 0.0025, 0.00325, 0.004, 0.00475, 0.0055, 0.00625, 0.007, 0.00775, 0.0085, 0.00925, 0.01,
-                        0.31075, 0.3115, 0.31225, 0.313, 0.31375, 0.3145, 0.31525, 0.316, 0.31675, 0.3175, 0.31825, 0.319, 0.31975, 
-                        0.3205, 0.32125, 0.322, 0.32275, 0.3235, 0.32425, 0.325, 0.32575, 0.3265, 0.32725, 0.328, 0.32875, 0.3295, 
-                        0.33025, 0.331, 0.33175, 0.3325, 0.33325, 0.334, 0.33475, 0.3355, 0.33625, 0.337, 0.33775, 0.3385, 0.33925, 0.34]
+start = 0.00025
+end = 0.34
+# n = 401
+# step = (end - start) / (n - 1)
+step = 0.00075
+n = int((end - start) / step + 1)
+print(n, type(n))
+FLEX_STRENGTH_values = [round(start + i * step, 5) for i in range(n)]
+# FLEX_STRENGTH_values = [0.00025, 0.001, 0.00175, 0.0025, 0.00325, 0.004, 0.00475, 0.0055, 0.00625, 0.007, 0.00775, 0.0085, 0.00925, 0.01,
+#                         0.31075, 0.3115, 0.31225, 0.313, 0.31375, 0.3145, 0.31525, 0.316, 0.31675, 0.3175, 0.31825, 0.319, 0.31975, 
+#                         0.3205, 0.32125, 0.322, 0.32275, 0.3235, 0.32425, 0.325, 0.32575, 0.3265, 0.32725, 0.328, 0.32875, 0.3295, 
+#                         0.33025, 0.331, 0.33175, 0.3325, 0.33325, 0.334, 0.33475, 0.3355, 0.33625, 0.337, 0.33775, 0.3385, 0.33925, 0.34]
 S_values = [1.0]
 
 def encode_s(x):
@@ -69,10 +69,10 @@ def encode_s(x):
 def encode_f(x):
     return f"{x:5f}".replace('.', 'p')
 
-DIR_base = "critical/SQUARE"
+DIR_base = "f_phase/SQUARE"
 
 # === PARALLELISM SETTINGS ===
-MAX_PARALLEL = 54  # run 30 simulations at the same time
+MAX_PARALLEL = 57  # run 30 simulations at the same time
 
 # === BUILD ALL PARAMETER COMBINATIONS ===
 combos = list(itertools.product(S_values, FLEX_STRENGTH_values))
